69aadcdc22bfab3b31ce01b4e51848024c9f7eb8
AlexRobbin <158841503+AlexRobbin@users.noreply.github.com>
2024-02-20 20:47:57 +0330

    Add files via upload

---
+++ for.py
@@ -0,0 +1,49 @@
+#............for..............
+'''
+num = [22 ,35 ,45 ,69 ,78]
+
+name = "reza"
+
+rangeofnum = range(0 ,10 ,2)
+
+for x in num :
+    print(x)
+
+print("__________________________")
+
+id = "mohammad"
+for count in id:
+    print(count)
+
+print("__________________________")
+
+for k in rangeofnum:
+    print(k)
+
+print("__________________________")
+
+print(list(rangeofnum))
+print("__________________________")
+
+print(list(range(10 , 0 , -2)))
+
+#...........................end.................
+
+
+v = range(0,10)
+for star in v:
+    print("*" * star)
+
+'''
+for num in range(0,10):
+    for count in range(0,10):
+        if count % 2 ==0:
+            print("*" * count)
+        else:
+            print("*" * count)
+    for count in range(10,0,-1):
+        if count % 2 ==0:
+            print("*" * count)
+        else:
+            print("*" * count)
+
